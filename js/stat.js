4226c6428d6184bdc814e453a1e023f612900f6f
alex-y-kuznetsov <alex.y.kuznetsov@gmail.com>
2017-01-23 22:15:21 +0500

    Добавил таблицу статистики

---
+++ js/stat.js
@@ -0,0 +1,50 @@
+'use strict';
+
+window.renderStatistics = function (ctx, names, times) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(110, 20, 420, 270);
+
+  ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
+  ctx.strokeRect(100, 10, 420, 270);
+  ctx.fillRect(100, 10, 420, 270);
+
+  ctx.fillStyle = 'rgba(0, 0, 0, 1.0)';
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Ура, Вы победили!', 120, 40);
+  ctx.fillText('Список результатов:', 120, 60);
+
+  var max = -1;
+  var min = Infinity;
+
+  times.forEach(function (time) {
+    if (time > max) {
+      max = time;
+    }
+    if (time < min) {
+      min = time;
+    }
+  });
+
+  var histoHeight = 150;
+  var histoX = 140;
+  var step = histoHeight / (max - min);
+  var columnIndent = 90;
+
+  for(var i = 0; i < times.length; i++) {
+    var name = names[i];
+    var time = Math.round(times[i]);
+    var height = step * (time - min);
+
+    if (name === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1.0)';
+    } else {
+      ctx.fillStyle = ['rgba(0, 0, 255, ', (Math.random()).toFixed(1), ')'].join('');
+    }
+    ctx.fillText(time.toFixed(0), histoX + columnIndent * i, 90 + histoHeight - height);
+    ctx.fillRect(histoX + columnIndent * i, 100 + histoHeight - height, 40, height);
+    ctx.fillText(name, histoX + columnIndent * i, 100 + histoHeight + 20);
+  }
+};
+
+var canvas = document.querySelector('canvas');
+window.renderStatistics(canvas.getContext('2d'), ['Вы', 'Кекс', 'Катя', 'Игорь'], [10.32, 40.11, 30.18, 25.25]);
